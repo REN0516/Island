32fa8326cc5d9f28e1b18788f7f81da0b3a2d63e
Shimoyama <odatomo0408@gmail.com>
2019-05-20 14:28:47 +0900

    Title遷移

---
+++ Project shima/Assets/Scripts/SceneSelectManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectManager : MonoBehaviour
+{
+    public void PushPlayButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}

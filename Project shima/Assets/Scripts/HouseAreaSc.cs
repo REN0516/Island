2369a5bc55877f9cab2ebd63018ee79c98ad95b2
REN <ren.numao0516@gmail.com>
2019-06-10 16:01:49 +0900

    プレイヤーモデル追加

---
+++ Project shima/Assets/Scripts/HouseAreaSc.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HouseAreaSc : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "HouseArea")
+        {
+            SceneManager.LoadScene("House");
+        }
+    }
+
+
+}

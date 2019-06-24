7ae947b002da306853ba6a07a0c961888c69e3d5
Ren_Numao <ren.numao0516@gmail.com>
2019-06-24 21:30:02 +0900

    キャラクターアニメーション（run、Idol）カメラ追加

--- Project shima/Assets/Scripts/PlayerMove.cs
+++ Project shima/Assets/Scripts/PlayerMove.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public static PlayerMove scPlayerMove;
-
-    static float inputHorizontal;
-    static float inputVertical;
-    Rigidbody rb;
     private Animator animator;
-
+    float animS = 0.1f;
+    float inputHorizontal;
+    float inputVertical;
     float moveSpeed = 3f;
+    Rigidbody playerRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-       animator = GetComponent<Animator>();
+        playerRB = GetComponent<Rigidbody>();
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.anyKey)
         {
             animator.SetBool("run", true);
         }
@@ -30,24 +33,11 @@ public class PlayerMove : MonoBehaviour
         {
             animator.SetBool("run", false);
         }
-        // animator.SetBool("FastRun", false);
-        /*if (Input.GetKey(KeyCode.UpArrow)) {
 
-            //inputHorizontal = Input.GetAxisRaw("Horizontal");
-            animator.SetBool("FastRun",true);
-           
-        }
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            //inputVertical = Input.GetAxisRaw("Vertical");
-            animator.SetBool("FastRun", true);
-        }*/
-        inputHorizontal = Input.GetAxisRaw("Horizontal");
-        inputVertical = Input.GetAxisRaw("Vertical");
 
     }
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-    
         // カメラの方向から、X-Z平面の単位ベクトルを取得
         Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
 
@@ -55,7 +45,7 @@ public class PlayerMove : MonoBehaviour
         Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;
 
         // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
-        rb.velocity = moveForward * moveSpeed + new Vector3(0, rb.velocity.y, 0);
+        playerRB.velocity = moveForward * moveSpeed + new Vector3(0, playerRB.velocity.y, 0);
 
         // キャラクターの向きを進行方向に
         if (moveForward != Vector3.zero)
@@ -63,29 +53,4 @@ public class PlayerMove : MonoBehaviour
             transform.rotation = Quaternion.LookRotation(moveForward);
         }
     }
-
-
-    void Awake()
-    {
-        //スクリプトが設定されていなければゲームオブジェクトを残しつつスクリプトを設定
-        if(scPlayerMove == null)
-        {
-            DontDestroyOnLoad(gameObject);
-            scPlayerMove = this;
-            
-        }
-        //既にPlayerMoveスクリプトがあればこのシーンの同じゲームオブジェクトを削除
-        
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-    public static GameObject GetPlayer()
-    {
-        return scPlayerMove.gameObject;
-    }
-/*
-*/
-
 }
